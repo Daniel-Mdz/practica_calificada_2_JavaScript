124b17ada4573dfec9a92405126cd0851e25e3c2
Daniel-Mdz <danielrandiss@gmail.com>
2024-07-30 23:14:34 -0500

    7 y 9 - Jenciso

--- ejer_7.js
+++ ejer_7.js
@@ -4,3 +4,20 @@
 // devuelva un nuevo array con las palabras que tengan más letras que el número 
 // especificado. 
 // Prueba la función filtrarPalabras y muestra el resultado en la consola.
+// 1. Definir el array de palabras
+let palabras = ["gato", "perro", "elefante", "ratón", "jirafa"];
+
+// 2. Crear una función que filtre palabras por longitud
+function filtrarPalabras(array, numero) {
+    let resultado = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > numero) {
+            resultado.push(array[i]);
+        }
+    }
+    return resultado;
+}
+
+// 3. Probar la función y mostrar el resultado en la consola
+let palabrasFiltradas = filtrarPalabras(palabras, 4);
+console.log(palabrasFiltradas); // Imprime las palabras que tienen más de 4 letras}
