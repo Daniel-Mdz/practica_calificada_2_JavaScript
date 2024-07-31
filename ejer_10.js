9a328e1fcdfb8e980ccb97b986e117dc2fb09088
ItalyArano <italy-bb@hotmail.com>
2024-07-30 22:17:25 -0600

    Ejercicio_10

--- ejer_10.js
+++ ejer_10.js
@@ -2,3 +2,16 @@
 // Crea una función llamada revertirCadena que reciba una cadena de texto y 
 // devuelva la cadena invertida. 
 // Prueba la función revertirCadena y muestra el resultado en la consola.
+// Definimos la función revertirCadena
+function revertirCadena(cadena) {
+    // Convertimos la cadena en un array, invertimos el array y lo unimos de nuevo en una cadena
+    return cadena.split('').reverse().join('');
+  }
+  
+  // Probamos la función con una cadena de ejemplo
+  const textoOriginal = "Hola Mundo";
+  const textoRevertido = revertirCadena(textoOriginal);
+  
+  // Mostramos el resultado en la consola
+  console.log(textoOriginal)
+  console.log(textoRevertido);
