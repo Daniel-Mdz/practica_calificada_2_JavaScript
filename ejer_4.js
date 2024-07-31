d500afb679eda3b08bd40e44785465e2a5ea69a1
JuanGeldres <geldresjuan36@gmail.com>
2024-07-30 23:37:36 -0500

    mejorado

--- ejer_4.js
+++ ejer_4.js
@@ -4,3 +4,14 @@
 // argumentos y devuelva un nuevo array que sea la concatenación de los dos. 
 // Llama a la función concatenarArrays con array1 y array2 y muestra el resultado 
 // en la consola.
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+
+function concatenarArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+
+const resultado = concatenarArrays(array1, array2);
+
+console.log("El resultado es:",resultado);
